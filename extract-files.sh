cfb3786e8d04a4f4ee845d3f803c78d10360fe93
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-08 18:07:30 +0300

    zeekr: move libancbase_rt_fusion.so SO name fixup to common
    Change-Id: Icfa0a8ed919ee38a585551d1066b85e67fc81374

--- extract-files.sh
+++ extract-files.sh
@@ -8,9 +8,6 @@
 
 function blob_fixup() {
     case "${1}" in
-        vendor/lib64/libancbase_rt_fusion.so)
-            ${PATCHELF} --set-soname libancbase_rt_fusion.so "${2}"
-            ;;
         vendor/lib/libcamximageformatutils.so | vendor/lib64/libcamximageformatutils.so)
             ${PATCHELF} --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
             ;;

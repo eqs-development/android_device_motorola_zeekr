b822871eff41cba4ae31225b69b5ae64a9d94a7d
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-08 18:07:54 +0300

    zeekr: remove duplicate blob fixup
    Already exists in common.
    
    Change-Id: I8164081c1a4bd036a0fa19be3da6bfd0978fb16e

--- extract-files.sh
+++ extract-files.sh
@@ -6,14 +6,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-function blob_fixup() {
-    case "${1}" in
-        vendor/lib/libcamximageformatutils.so | vendor/lib64/libcamximageformatutils.so)
-            ${PATCHELF} --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
-            ;;
-    esac
-}
-
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
